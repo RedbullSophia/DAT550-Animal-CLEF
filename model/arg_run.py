cf525e5c97337ad9540abf1140eac1811611d5eb
AleksanderIxys <amo@ixys.no>
2025-04-01 19:37:26 +0200

    hf

--- model/arg_run.py
+++ model/arg_run.py
@@ -92,8 +92,8 @@ if __name__ == "__main__":
     setup_logging(save_path)  # Initialize logging with the dynamic path
 
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    #DATA_ROOT = '/home/stud/aleks99/.cache/kagglehub/datasets/wildlifedatasets/wildlifereid-10k/versions/6'
-    DATA_ROOT = 'C:/Users/trade/.cache/kagglehub/datasets/wildlifedatasets/wildlifereid-10k/versions/6'
+    DATA_ROOT = '/home/stud/aleks99/.cache/kagglehub/datasets/wildlifedatasets/wildlifereid-10k/versions/6'
+    #DATA_ROOT = 'C:/Users/trade/.cache/kagglehub/datasets/wildlifedatasets/wildlifereid-10k/versions/6'
 
     logging.info(f"Using device: {DEVICE}")
     if torch.cuda.is_available():
