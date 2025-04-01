6e22ee43fa15761a02e02411cc865d003c258631
Morten <maadvendt@gmail.com>
2025-04-01 22:18:29 +0200

    testing

--- shell_scripts/train2.sh
+++ shell_scripts/train2.sh
@@ -14,10 +14,10 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # Run your Python script with args
 python -u ../model/arg_run.py \
-  --backbone resnet50 \
-  --batch_size 6144 \
-  --num_epochs 50 \
+  --backbone resnet18 \
+  --batch_size 4096 \
+  --num_epochs 10 \
   --lr 0.001 \
   --m 3 \
   --resize 160 \
-  --n 140000
+  --n 30000
