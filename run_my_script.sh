a79a57e1fd20d193c4acbc308b8b6ad830867eb7
Aleksander Møllerud <aleks99@ux.uis.no>
2025-04-01 11:54:00 +0200

    trente full modell

--- run_my_script.sh
+++ run_my_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:0
 #SBATCH --partition=gpu
-#SBATCH --time=02:15:00
+#SBATCH --time=20:15:00
 #SBATCH --job-name=runscript
 #SBATCH --output=ran.out
 
@@ -9,15 +9,14 @@
 uenv verbose cuda-11.4.4 cudnn-11.x-8.8.0
 uenv miniconda3-py311
 python --version
-#pip install -r requirements4.txt
-#pip install --force-reinstall numpy==2.2.4 scikit-learn==1.6.1 pandas==2.2.3 bottleneck
-#pip install bottleneck --upgrade
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # Run your Python script with args
 python -u model/arg_run.py \
-  --batch_size 8192 \
+  --batch_size 6144 \
   --num_epochs 10 \
   --lr 0.0005 \
-  --m 4 \
-  --resize 210 \
-  --n 40000
+  --m 3 \
+  --resize 160 \
+  --n 140000
